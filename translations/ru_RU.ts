7fdb5b0645931f43971d5790063a564d6c1ef85a
Rinat Namazov <namazov.rinat@gmail.com>
2023-09-24 10:03:51 +0300

    Add Russian language

---
+++ translations/ru_RU.ts
@@ -0,0 +1,467 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AdaptersDialog</name>
+    <message>
+        <location filename="../src/AdaptersDialog.ui" line="14"/>
+        <source>Adapters</source>
+        <translation type="unfinished">Сетевые адаптеры</translation>
+    </message>
+    <message>
+        <location filename="../src/AdaptersDialog.ui" line="48"/>
+        <source>Address</source>
+        <translation type="unfinished">Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/AdaptersDialog.ui" line="64"/>
+        <source>Add</source>
+        <translation type="unfinished">Добавить</translation>
+    </message>
+    <message>
+        <location filename="../src/AdaptersDialog.ui" line="74"/>
+        <source>Delete</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../src/AdaptersDialog.ui" line="96"/>
+        <source>It only works with the RiNetworkAdapter.asi installed, can be used for a VPN.</source>
+        <translation type="unfinished">Работает только с установленным RiNetworkAdapter.asi, используется для VPN.</translation>
+    </message>
+</context>
+<context>
+    <name>GameLauncher</name>
+    <message>
+        <location filename="../src/GameLauncher.cpp" line="28"/>
+        <location filename="../src/GameLauncher.cpp" line="37"/>
+        <source>Error</source>
+        <translation type="unfinished">Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/GameLauncher.cpp" line="118"/>
+        <location filename="../src/GameLauncher.cpp" line="127"/>
+        <location filename="../src/GameLauncher.cpp" line="134"/>
+        <source>Failed to start the game</source>
+        <translation type="unfinished">Не удалось запустить игру</translation>
+    </message>
+    <message>
+        <location filename="../src/GameLauncher.cpp" line="181"/>
+        <source>Failed to inject the library</source>
+        <translation type="unfinished">Не удалось заинжектить библиотеку</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.ui" line="17"/>
+        <source>SampX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="41"/>
+        <source>Group:</source>
+        <translation type="unfinished">Группа:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="55"/>
+        <source>Search:</source>
+        <translation type="unfinished">Поиск:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="78"/>
+        <source>Add group</source>
+        <translation type="unfinished">Добавить группу</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="85"/>
+        <source>Delete group</source>
+        <translation type="unfinished">Удалить группу</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="94"/>
+        <location filename="../src/MainWindow.cpp" line="220"/>
+        <location filename="../src/MainWindow.cpp" line="795"/>
+        <source>Add server</source>
+        <translation type="unfinished">Добавить сервер</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="127"/>
+        <source>HostName</source>
+        <translation type="unfinished">Имя хоста</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="132"/>
+        <source>Players</source>
+        <translation type="unfinished">Игроков</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="137"/>
+        <source>Ping</source>
+        <translation type="unfinished">Пинг</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="142"/>
+        <source>Mode</source>
+        <translation type="unfinished">Мод</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="147"/>
+        <location filename="../src/MainWindow.ui" line="584"/>
+        <source>Language</source>
+        <translation type="unfinished">Язык</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="152"/>
+        <source>Address</source>
+        <translation type="unfinished">Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="170"/>
+        <source>Server Information</source>
+        <translation type="unfinished">Информация о сервере</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="178"/>
+        <source>Address:</source>
+        <translation type="unfinished">Адрес:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="195"/>
+        <source>Players:</source>
+        <translation type="unfinished">Игроков:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="212"/>
+        <source>Ping:</source>
+        <translation type="unfinished">Пинг:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="229"/>
+        <source>Mode:</source>
+        <translation type="unfinished">Мод:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="246"/>
+        <source>Language:</source>
+        <translation type="unfinished">Язык:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="267"/>
+        <source>Map:</source>
+        <translation type="unfinished">Карта:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="284"/>
+        <source>Weather:</source>
+        <translation type="unfinished">Погода:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="301"/>
+        <source>WorldTime:</source>
+        <translation type="unfinished">Время:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="318"/>
+        <source>URL:</source>
+        <translation type="unfinished">Сайт:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="338"/>
+        <source>Version:</source>
+        <translation type="unfinished">Версия:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="382"/>
+        <source>Profile:</source>
+        <translation type="unfinished">Профиль:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="396"/>
+        <source>NickName:</source>
+        <translation type="unfinished">Никнейм:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="410"/>
+        <source>Works only with RiProxy.asi installed.</source>
+        <translation type="unfinished">Работает только с установленным RiProxy.asi.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="413"/>
+        <source>Proxy:</source>
+        <translation type="unfinished">Прокси:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="427"/>
+        <source>Works only with RiNetworkAdapter.asi installed, can be used for a VPN.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Works only with RiNetworkAdapter.asi installed, can be used for a VPN</source>
+        <translation type="obsolete">Работает только с установленным RiNetworkAdapter.asi, используется для VPN</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="430"/>
+        <source>Adapter:</source>
+        <translation type="unfinished">Адаптер:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="440"/>
+        <source>Game dir:</source>
+        <translation type="unfinished">Папка игры:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="462"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="481"/>
+        <source>GTA:SA:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="488"/>
+        <source>SA-MP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="495"/>
+        <source>Comment:</source>
+        <translation type="unfinished">Комментарий:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="518"/>
+        <source>Connect</source>
+        <translation type="unfinished">Подключиться</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="527"/>
+        <source>Create new profile</source>
+        <translation type="unfinished">Создать новый профиль</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="540"/>
+        <source>Delete profile</source>
+        <translation type="unfinished">Удалить профиль</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="564"/>
+        <source>Help</source>
+        <translation type="unfinished">Помощь</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="573"/>
+        <source>Settings</source>
+        <translation type="unfinished">Настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="577"/>
+        <source>Theme</source>
+        <translation type="unfinished">Тема</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="598"/>
+        <source>Check for updates</source>
+        <translation type="unfinished">Проверить обновления</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="603"/>
+        <location filename="../src/MainWindow.cpp" line="616"/>
+        <source>Site</source>
+        <translation type="unfinished">Сайт</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="608"/>
+        <source>About</source>
+        <translation type="unfinished">О нас</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="613"/>
+        <source>About Qt</source>
+        <translation type="unfinished">О Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="621"/>
+        <source>Proxy</source>
+        <translation type="unfinished">Прокси</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="629"/>
+        <source>VPN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="634"/>
+        <source>SA-MP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="642"/>
+        <source>Dark</source>
+        <translation type="unfinished">Темная</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="650"/>
+        <source>Light</source>
+        <translation type="unfinished">Светлая</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="655"/>
+        <source>Adapter (VPN)</source>
+        <translation type="unfinished">Адаптер (VPN)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="175"/>
+        <source>Add to</source>
+        <translation type="unfinished">Добавить в</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="184"/>
+        <source>Delete</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="185"/>
+        <source>Edit password</source>
+        <translation type="unfinished">Изменить пароль</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="221"/>
+        <source>Enter server password</source>
+        <translation type="unfinished">Введите пароль сервера</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="314"/>
+        <location filename="../src/MainWindow.cpp" line="712"/>
+        <source>Not Available</source>
+        <translation type="unfinished">Не доступно</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="598"/>
+        <source>SampX About</source>
+        <translation type="unfinished">О SampX</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="613"/>
+        <source>Version</source>
+        <translation type="unfinished">Версия</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="615"/>
+        <source>The custom launcher of SA-MP</source>
+        <translation type="unfinished">Кастомный лаунчер для SA-MP</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="617"/>
+        <source>Developer</source>
+        <translation type="unfinished">Разработчик</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="618"/>
+        <source>Donate</source>
+        <translation type="unfinished">Донат</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="773"/>
+        <source>Are you sure you want to delete the current group?</source>
+        <translation type="unfinished">Вы уверены, что хотите удалить текущую группу?</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="796"/>
+        <source>Enter new server host:port</source>
+        <translation type="unfinished">Введите адрес нового сервера</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="878"/>
+        <location filename="../src/MainWindow.cpp" line="1167"/>
+        <source>Server Information: </source>
+        <translation type="unfinished">Информация о сервере: </translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="999"/>
+        <source>Select the directory with the game</source>
+        <translation type="unfinished">Выберите папку с игрой</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="1246"/>
+        <source>You have an empty nickname.</source>
+        <translation type="unfinished">У вас пустой никнейм.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="1252"/>
+        <source>You have an empty game directory.</source>
+        <translation type="unfinished">У вас пустой путь к папке с игрой.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="1258"/>
+        <source>You have an empty game executable.</source>
+        <translation type="unfinished">Вы не выбрали файл игры.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="1264"/>
+        <source>You have an empty samp version.</source>
+        <translation type="unfinished">Вы не выбрали версию сампа.</translation>
+    </message>
+</context>
+<context>
+    <name>ProxysDialog</name>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="14"/>
+        <source>Proxys</source>
+        <translation type="unfinished">Прокси</translation>
+    </message>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="43"/>
+        <source>Address</source>
+        <translation type="unfinished">Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="48"/>
+        <source>Key</source>
+        <translation type="unfinished">Ключ</translation>
+    </message>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="64"/>
+        <source>Add</source>
+        <translation type="unfinished">Добавить</translation>
+    </message>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="74"/>
+        <source>Delete</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../src/ProxysDialog.ui" line="96"/>
+        <source>It only works with the RiProxy.asi installed.</source>
+        <translation type="unfinished">Работает только с установленным RiProxy.asi.</translation>
+    </message>
+</context>
+<context>
+    <name>SampVersionsDialog</name>
+    <message>
+        <location filename="../src/SampVersionsDialog.ui" line="14"/>
+        <source>SA-MP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/SampVersionsDialog.ui" line="34"/>
+        <source>Add</source>
+        <translation type="unfinished">Добавить</translation>
+    </message>
+    <message>
+        <location filename="../src/SampVersionsDialog.ui" line="44"/>
+        <source>Delete</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../src/SampVersionsDialog.ui" line="95"/>
+        <source>Version Name</source>
+        <translation type="unfinished">Версия</translation>
+    </message>
+    <message>
+        <location filename="../src/SampVersionsDialog.ui" line="100"/>
+        <source>File Name</source>
+        <translation type="unfinished">Файл</translation>
+    </message>
+</context>
+</TS>
